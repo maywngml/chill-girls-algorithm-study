231e8b275689c91d0f740aeb690ac541fddb2d3f
devGO <been2zz@naver.com>
2025-03-20 17:06:39 +0900

    [Solve] ✍ 1149.RGB거리

---
+++ Week01/1149_eb.js
@@ -0,0 +1,15 @@
+if (n === 1) {
+  console.log(Math.min(...matrix[0]));
+}
+let dp = Array(n).fill().map(() => [0, 0, 0]);
+
+dp[0][0] = matrix[0][0]
+dp[0][1] = matrix[0][1]
+dp[0][2] = matrix[0][2]
+for (let i = 1; i < n; i++) {
+  dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + matrix[i][0]
+  dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + matrix[i][1]
+  dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + matrix[i][2]
+}
+console.log(Math.min(...[dp[n - 1][0], dp[n - 1][1], dp[n - 1][2]]))
+
