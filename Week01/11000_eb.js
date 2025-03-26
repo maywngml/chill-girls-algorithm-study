504d72158c2b3d5575d30b51e0aaf787f406130b
devGO <been2zz@naver.com>
2025-03-26 09:35:45 +0900

    update: 피드백백

--- Week01/11000_eb.js
+++ Week01/11000_eb.js
@@ -1,13 +1,9 @@
 const n = 3;
-const classes = [
-  [1, 3],
-  [2, 4],
-  [3, 5]]
-
+const classes = [[1, 2], [3, 4], [4, 5]];
 const startTimes = classes.map(c => c[0]).sort((a, b) => a - b);
 const endTimes = classes.map(c => c[1]).sort((a, b) => a - b);
 let classRoom = 0;
-let endIndex = 0;
+let endIndex = 0; // 현재까지 종료된 수업의 인덱스
 
 for (let i = 0; i < endTimes.length; i++) {
   if (startTimes[i] < endTimes[endIndex]) {
@@ -17,4 +13,4 @@ for (let i = 0; i < endTimes.length; i++) {
   }
 }
 
-console.log(classRoom)
+console.log(classRoom);
--- Week01/1149_eb.js
+++ Week01/1149_eb.js
@@ -1,15 +1,16 @@
+let n = 3;
+let matrix = [[26, 40, 83], [49, 60, 57], [13, 89, 99]];
 if (n === 1) {
   console.log(Math.min(...matrix[0]));
 }
-let dp = Array(n).fill().map(() => [0, 0, 0]);
+let dp = Array(n).fill().map(() => Array(3).fill(0));
 
-dp[0][0] = matrix[0][0]
-dp[0][1] = matrix[0][1]
-dp[0][2] = matrix[0][2]
+dp[0][0] = matrix[0][0];
+dp[0][1] = matrix[0][1];
+dp[0][2] = matrix[0][2];
 for (let i = 1; i < n; i++) {
-  dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + matrix[i][0]
-  dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + matrix[i][1]
-  dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + matrix[i][2]
+  dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + matrix[i][0];
+  dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + matrix[i][1];
+  dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + matrix[i][2];
 }
-console.log(Math.min(...[dp[n - 1][0], dp[n - 1][1], dp[n - 1][2]]))
-
+console.log(Math.min(...dp[n - 1]));
